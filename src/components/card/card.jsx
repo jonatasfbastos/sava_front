25b08dc7fb5534ba7346caae14cc22d3fb41c4f3
sheory <sheoryd@gmail.com>
2021-11-11 22:14:48 -0300

    SI-2 adding more fake data to class council cards

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -20,7 +20,37 @@ const cardItems = [
       classYear: "3º Ano B",
       status: "Fechado",
       quarter: "2020.3"
-  }
+  },
+	{
+		subject: "Eletromecânica",
+		classYear: "1º Ano C",
+		status: "Aberto",
+		quarter: "2021.2"
+	},
+	{
+		subject: "Biocombustíveis",
+		classYear: "1º Ano C",
+		status: "Fechado",
+		quarter: "2020.2"
+	}, 
+	{
+		subject: "Biocombustíveis",
+		classYear: "2º Ano A",
+		status: "Fechado",
+		quarter: "2020.3"
+	},
+	{
+		subject: "Eletromecânica",
+		classYear: "3º Ano B",
+		status: "Fechado",
+		quarter: "2020.3"
+	},
+	{
+		subject: "Informática",
+		classYear: "2º Ano A",
+		status: "Aberto",
+		quarter: "2020.2"
+	},
 ]
 
 const Card = () => {
