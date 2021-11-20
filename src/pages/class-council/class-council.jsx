3f08ffe503a28651a6e801ceab4f52091f22cabd
igorttosta <matositosta@gmail.com>
2021-11-20 16:25:20 -0300

    SI-21 add topbar and button

--- src/pages/class-council/class-council.jsx
+++ src/pages/class-council/class-council.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Card from '../../components/card/card';
+import TopBar from '../../components/top-bar/top-bar';
 
 const ClassCouncil = () => {
-    return <div>
-        <Card/>
+    return <div className="container-class-council">
+        <TopBar icon="bi bi-people" path="Conselho de Classe" typeButton="class-council"/>
+        <div className="cards-container-class-council">
+            <Card/>
+        </div>
     </div>
 }
 
--- src/pages/question-bank-page/question-bank-page.jsx
+++ src/pages/question-bank-page/question-bank-page.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import QuestionsBank from '../../components/questions-bank/questions-bank';
+import TopBar from '../../components/top-bar/top-bar';
 
 const QuestionBank = () => {
     return <div> 
+        <TopBar icon="bi bi-question-circle" path="Banco de Perguntas" typeButton="question-bank"/>
         <QuestionsBank/>
     </div>
 }
