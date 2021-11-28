dde481481daffd931c5749a027a8b1003fb06a15
brunomartin5 <martsbrunoo@gmail.com>
2021-11-28 12:13:00 -0300

    SI-22 create footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './footer.css';
+
+const Footer = () =>{
+    return(
+        <div className = "footer">
+            <div className = "body">
+                
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
