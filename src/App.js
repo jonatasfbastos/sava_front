f22befa14ef306bd05b412155f30834581c0a655
sheory <sheoryd@gmail.com>
2021-11-28 12:03:07 -0300

    SI-31 add cotexttttt uhuuu

--- src/App.js
+++ src/App.js
@@ -1,45 +1,18 @@
 import React, {useState} from 'react';
 
 import './App.css';
-import ClassCouncil from './pages/class-council/class-council';
-import QuestionBank from './pages/question-bank-page/question-bank-page';
-import RecoverPassword from './pages/recover-password/recover-password';
-import Login from './pages/login/login';
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom";
-
+import SideBarRoutes from './routes/routes';
 import SideBar from './components/side-bar/side-bar';
+import { SideBarProvider } from './context/side-bar-context';
 
-
-const Home = () => {
-  return <h1>Ínicio</h1>
-}
 
 function App() {
-  const [inactive, setInactive] = useState(false)
-
   return (    
     <div className="App">
-      <Router>
-        <SideBar onCollapse={(inactive) => {
-          setInactive(inactive)
-        }} />
-
-        <div className={`container${inactive ? " inactive" : ""} `}>
-          <Routes>
-            <Route exact path={'/'} element={<Home />} />
-            <Route path={'/conselho_de_classe'} element={<ClassCouncil/>} />
-            <Route path={'/banco_de_perguntas'} element={<QuestionBank/>} />
-            <Route path={'/recuperar_senha'} element={<RecoverPassword/>} />
-            <Route path={'/login'} element={<Login/>} />
-          </Routes>
-        </div>
-      </Router>
+      <SideBarProvider>
+        <SideBarRoutes />
+      </SideBarProvider>
     </div>
   );
 }
