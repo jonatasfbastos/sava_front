5ece195612ab2012d674e87502220510785ac7b9
sheory <sheoryd@gmail.com>
2021-12-11 14:28:05 -0300

    SI-51 fix modal uhuuu

--- src/components/modal-class-council/modal-class-council.jsx
+++ src/components/modal-class-council/modal-class-council.jsx
@@ -16,52 +16,58 @@ const ModalClassCouncil = () => {
     return (
     <div className="modal-background">
         <div className="modal-container">
-            <div className="modal-top">
-                <div className="modal-title">
-                    <span>Novo Conselho</span>
-                </div>
-                <div className="close-buttom" >
-                    <i onClick={() => setOpenModal(false)} class="bi bi-x"></i>
-                </div>
-            </div>
-            <div className="dropdown-container">
-                <Dropdown type="class"/>
-            </div>   
-            <div className="questionnaire-conteiner">        
-                <span id="questionnaire">Questionário • Professor:</span>
-                <div className="modal-container-body" >
-                    <div className="questions-container" style={{overflowY: 'scroll', height: '100%'}}>
-                        {dataTeacher.map((item, index) => (
-                            <QuestionItem
-                                number={item.id}
-                                question={item.question}
-                            />
-
-                        ))}
+            <div className="modal-container-superior" style={{overflowY: 'scroll', height: '100%'}}>
+                <div className="modal-top">
+                    <div className="modal-title">
+                        <span>Novo Conselho</span>
+                    </div>
+                    <div className="close-buttom" >
+                        <i onClick={() => setOpenModal(false)} class="bi bi-x"></i>
                     </div>
                 </div>
-                <span id="questionnaire">Questionário • Aluno:</span>
-                <div className="modal-container-body">
-                    <div className="questions-container" style={{overflowY: 'scroll', height: '100%'}}>
-                        {dataStudent.map((item, index) => (
-                            <QuestionItem
-                                number={item.id}
-                                question={item.question}
-                            />
 
-                        ))}
+                <div className="dropdown-container">
+                    <Dropdown type="class"/>
+                </div>   
+
+                <div className="questionnaire-conteiner">        
+                    <span id="questionnaire">Questionário • Professor:</span>
+                    <div className="modal-container-body" >
+                        <div className="questions-container" style={{overflowY: 'scroll', height: '100%'}}>
+                            {dataTeacher.map((item, index) => (
+                                <QuestionItem
+                                    number={item.id}
+                                    question={item.question}
+                                />
+
+                            ))}
+                        </div>
+                    </div> 
+
+                    <span id="questionnaire">Questionário • Aluno:</span>
+                    <div className="modal-container-body">
+                        <div className="questions-container" style={{overflowY: 'scroll', height: '100%'}}>
+                            {dataStudent.map((item, index) => (
+                                <QuestionItem
+                                    number={item.id}
+                                    question={item.question}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </div> 
+            </div>
+                <div className="modal-container-bottom">
+                    <div className="container-buttons-bottom">  
+                        <div className="body-button">       
+                            <button>Banco de Perguntas</button>
+                        </div>
+                        <div className="bot-button">
+                            <button id="cancel" onClick={() => {setOpenModal(false)}}> Cancelar </button>
+                            <button id="register" > Cadastrar </button>
+                        </div>
                     </div>
                 </div>
-            </div> 
-            <div className="modal-container-bottom">
-                <div className="body-button">       
-                    <button>Banco de Perguntas</button>
-                </div>
-                <div className="bot-button">
-                    <button id="cancel" onClick={() => {setOpenModal(false)}}> Cancelar </button>
-                    <button id="register" > Cadastrar </button>
-                </div>
-            </div>
         </div>
     </div>
     )
